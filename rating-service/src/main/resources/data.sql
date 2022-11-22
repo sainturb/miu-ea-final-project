b4d295efbbcd3b1a436d9aef29625301ab34f391
NaguaG <82986724+NaguaG@users.noreply.github.com>
2022-11-22 17:25:56 -0600

    commit new instance

---
+++ rating-service/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO Rating(id, motion_picture_id, motion_picture_type, rating, user_id)
+VALUES (1, 1, 'MOVIE', 5, 1);
+INSERT INTO Rating(id, motion_picture_id, motion_picture_type, rating, user_id)
+VALUES (2, 2, 'TVSHOW', 4, 2);
+INSERT INTO Rating(id, motion_picture_id, motion_picture_type, rating, user_id)
+VALUES (3, 3, 'MOVIE', 5, 3);
+INSERT INTO Rating(id, motion_picture_id, motion_picture_type, rating, user_id)
+VALUES (4, 4, 'TVSHOW', 5, 4);
+INSERT INTO Rating(id, motion_picture_id, motion_picture_type, rating, user_id)
+VALUES (5, 5, 'MOVIE', 3, 5);
+INSERT INTO Rating(id, motion_picture_id, motion_picture_type, rating, user_id)
+VALUES (6, 6, 'MOVIE', 3, 6);
