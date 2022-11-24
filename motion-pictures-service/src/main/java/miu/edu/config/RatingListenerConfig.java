44ad65fc9427d29a354c3ae4aab01f19901f831b
saintur_b <saintur.b@novelsoft.mn>
2022-11-23 21:36:11 -0600

    kafka communication for movie tvshow fix

--- motion-pictures-service/src/main/java/miu/edu/config/RatingListenerConfig.java
+++ motion-pictures-service/src/main/java/miu/edu/config/RatingListenerConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -21,12 +22,15 @@ import miu.edu.dto.RatingDTO;
 @EnableKafka
 public class RatingListenerConfig {
 
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
     @Bean
     public ConsumerFactory<String, RatingDTO> ratingConsumerFactory() {
 
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "group-1");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class);
