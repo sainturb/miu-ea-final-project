582195655bd356c67efd5be685c04fa802e24721
saintur_b <saintur.b@novelsoft.mn>
2022-11-23 20:40:19 -0600

    comment service security added

--- build.sh
+++ build.sh
@@ -1,17 +1,10 @@
 mvn clean install package
 
-docker build -t movie-service -f ./movie-service/Dockerfile .
-
-docker build -t tvshow-service -f ./tvshow-service/Dockerfile .
-
-docker build -t rating-service -f ./rating-service/Dockerfile .
-
+#docker build -t movie-service -f ./movie-service/Dockerfile .
+#docker build -t tvshow-service -f ./tvshow-service/Dockerfile .
+#docker build -t rating-service -f ./rating-service/Dockerfile .
 docker build -t comment-service -f ./comment-service/Dockerfile .
-
-docker build -t user-service -f  ./user-service/Dockerfile .
-
-docker build -t discovery-service -f  ./discovery-service/Dockerfile .
-
-docker build -t gateway-service -f  ./gateway-service/Dockerfile .
-
-docker build -t config-service -f  ./config-service/Dockerfile .
+#docker build -t user-service -f  ./user-service/Dockerfile .
+#docker build -t discovery-service -f  ./discovery-service/Dockerfile .
+#docker build -t gateway-service -f  ./gateway-service/Dockerfile .
+#docker build -t config-service -f  ./config-service/Dockerfile .
